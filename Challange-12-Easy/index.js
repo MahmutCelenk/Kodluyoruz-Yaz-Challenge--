a0ce685970f5dfb7b53e5dfe5ffa91cc8e9191b1
Mahmut Çelenk <95218926+MahmutCelenk@users.noreply.github.com>
2023-08-25 15:50:01 +0300

    Add files via upload

---
+++ Challange-12-Easy/index.js
@@ -0,0 +1,9 @@
+// Verilen Hız ve mesafe bilgileri
+let speed = 60;
+let distance = 240;
+
+// Hesaplama: Zaman = Mesafe / hız
+let time = distance / speed;
+
+
+console.log("Araba " + time + " Saatte " + distance + " Km yol Alır");
