29d77d1c703f747a0ed7b5641555eb693b29331c
Mahmut Çelenk <95218926+MahmutCelenk@users.noreply.github.com>
2023-08-25 17:48:04 +0300

    Add files via upload

---
+++ Challange-3-Medium/index.js
@@ -0,0 +1,10 @@
+let result = document.getElementById("result");
+let book = 80;
+let notebook = 20;
+let pencil = 5;
+
+let total = book * 2 + notebook * 3 + pencil * 4;
+
+
+
+result.innerHTML = `${"Toplam Tutar: "} ${total}`;
